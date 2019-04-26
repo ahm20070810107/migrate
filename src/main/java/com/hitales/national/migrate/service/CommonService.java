e43a6ff005290f5f33e83096ebfde0170de33e41
Hubot <ahm20070810107@163.com>
2019-04-26 15:41:35 +0800

    校验自然村

--- src/main/java/com/hitales/national/migrate/service/CommonService.java
+++ src/main/java/com/hitales/national/migrate/service/CommonService.java
@@ -2,6 +2,7 @@ package com.hitales.national.migrate.service;
 
 import com.google.common.base.Strings;
 import com.hitales.national.migrate.dao.DoctorClinicDao;
+import com.hitales.national.migrate.dao.GB2260Dao;
 import com.hitales.national.migrate.dao.OperatorDao;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Row;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -31,6 +33,8 @@ public class CommonService {
     private OperatorDao operatorDao;
     @Autowired
     private DoctorClinicDao doctorClinicDao;
+    @Autowired
+    private GB2260Dao gb2260Dao;
 
     @Autowired
     private PasswordEncoder passEncoder;
@@ -38,16 +42,23 @@ public class CommonService {
     int headIndex = 0;
 
     public boolean verify(String operatorSheet, String clinicSheet,String countySheet, String villageSheet){
+        if(Strings.isNullOrEmpty(operatorSheet) || Strings.isNullOrEmpty(clinicSheet) || Strings.isNullOrEmpty(countySheet) || Strings.isNullOrEmpty(villageSheet)){
+            throw new RuntimeException("自然村、行政县、医疗机构、运营用户的sheet名称均不能为空");
+        }
         SXSSFWorkbook verifyWorkbook = new SXSSFWorkbook(ExcelToolService.MAX_READ_SIZE);
         Set<String> villageSet = new HashSet<>();
         boolean operatorResult = verifyOperator(operatorSheet, verifyWorkbook);
+        boolean villageResult = verifyVillage(villageSheet,villageSet,verifyWorkbook);
         boolean clinicResult = verifyClinic(clinicSheet,villageSet,verifyWorkbook);
 
         excelToolService.saveExcelFile(verifyWorkbook, "公共信息");
-        return operatorResult && clinicResult;
+        return operatorResult && clinicResult && villageResult;
     }
 
     public void importToDb(String operatorSheet, String clinicSheet,String countySheet, String villageSheet){
+        if(Strings.isNullOrEmpty(operatorSheet) || Strings.isNullOrEmpty(clinicSheet) || Strings.isNullOrEmpty(countySheet) || Strings.isNullOrEmpty(villageSheet)){
+            throw new RuntimeException("自然村、行政县、医疗机构、运营用户的sheet名称均不能为空");
+        }
 
     }
 
@@ -69,11 +80,32 @@ public class CommonService {
             String govVillageCode = row.getCell(1).getStringCellValue();
             if(Strings.isNullOrEmpty(villageName) || Strings.isNullOrEmpty(govVillageCode)){
                 sb.append(count++).append("、自然村名称,所属行政村编码均不能为空！\r\n");
+            }else {
+                Long govVillageCodeL = null;
+
+                try {
+                    govVillageCodeL = Long.parseLong(govVillageCode);
+                } catch (NumberFormatException e) {
+                    sb.append(count++).append("、所属行政村编码必须是15位数字\r\n");
+                }
+                if(!Objects.isNull(govVillageCodeL) && gb2260Dao.findByCanonicalCodeAndDepth(govVillageCodeL, 5).size() < 1){
+                    sb.append(count++).append("、所属行政村编码在数据库中不存在或不为行政村级别\r\n");
+                }
+                if(!Objects.isNull(govVillageCodeL) && gb2260Dao.findByNameAndCanonicalCode(villageName,govVillageCodeL).size() > 0){
+                    sb.append(count++).append("、自然村名称在数据库相同行政村编码中有重复\r\n");
+                }
             }
+            if(villageSet.contains(villageName)){
+                sb.append(count++).append("、自然村名称在excel中有重复\r\n");
+            }
+            villageSet.add(villageName);
 
+            if(count.compareTo(1) > 0){
+                result = false;
+                Row verifyRow = verifySheet.createRow(verifyRowCount++);
+                fillSheetRow(i+1,verifyRow,villageName,govVillageCode,sb.toString());
+            }
         }
-
-
         return result;
     }
 
